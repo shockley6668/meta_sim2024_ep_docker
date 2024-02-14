24a46342c6291a852ed2e3e7c86323e61c4a186c
Burppp <1483543906@qq.com>
2024-02-14 16:53:12 +0800

    frame

--- ICRA2024-Sim2Real-RM/src/build/devel/share/rmus_solution/cmake/rmus_solution-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(rmus_solution_MESSAGE_FILES "")
-set(rmus_solution_SERVICE_FILES "/home/ubuntu/meta_sim2024_ep_docker/ICRA2024-Sim2Real-RM/src/rmus_solution/srv/setgoal.srv;/home/ubuntu/meta_sim2024_ep_docker/ICRA2024-Sim2Real-RM/src/rmus_solution/srv/switch.srv;/home/ubuntu/meta_sim2024_ep_docker/ICRA2024-Sim2Real-RM/src/rmus_solution/srv/graspsignal.srv")
--- ICRA2024-Sim2Real-RM/src/build/rmus_solution/catkin_generated/installspace/rmus_solution-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(rmus_solution_MESSAGE_FILES "")
-set(rmus_solution_SERVICE_FILES "srv/setgoal.srv;srv/switch.srv;srv/graspsignal.srv")
