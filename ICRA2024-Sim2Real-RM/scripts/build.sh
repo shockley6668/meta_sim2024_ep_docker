e14e2aea75d9f390d54305b36ecf073144a11c55
Burppp <136089129+Burppp@users.noreply.github.com>
2024-02-23 17:01:26 +0800

    1

--- ICRA2024-Sim2Real-RM/scripts/build.sh
+++ ICRA2024-Sim2Real-RM/scripts/build.sh
@@ -3,4 +3,4 @@ CLIENT_IMAGE=${CLIENT_IMAGE:-test}
 docker build . -t $CLIENT_IMAGE 
 
 
-# CLIENT_IMAGE=meta_sim/test:v1.6 bash scripts/build.sh
+# CLIENT_IMAGE=meta_sim/test:v1.7 bash scripts/build.sh
