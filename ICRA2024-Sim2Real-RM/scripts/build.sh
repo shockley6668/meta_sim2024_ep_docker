764e29f334f055f65f561e67c5bed3b26255dbdc
shockley+6668 <shukun6668@gmail.com>
2024-02-29 01:03:49 +0800

    u

--- ICRA2024-Sim2Real-RM/scripts/build.sh
+++ ICRA2024-Sim2Real-RM/scripts/build.sh
@@ -3,4 +3,4 @@ CLIENT_IMAGE=${CLIENT_IMAGE:-test}
 docker build . -t $CLIENT_IMAGE 
 
 
-# CLIENT_IMAGE=shockley/meta_sim:v2.0 bash scripts/build.sh
+# CLIENT_IMAGE=shockley/meta_sim:v2.1 bash scripts/build.sh
