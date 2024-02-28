3b28bb82479770c008990d34fe5e198b02739fff
Shockley <shukun6668@gmail.com>
2024-02-28 21:19:33 +0800

    update

---
+++ ICRA2024-Sim2Real-RM/src/bt_frame/include/timeout_node.h
@@ -0,0 +1,39 @@
+#include <chrono>
+#include "ros/ros.h"
+#include "behaviortree_cpp/bt_factory.h"
+using namespace BT;
+class Timeout : public StatefulActionNode
+{
+public:
+    Timeout(ros::NodeHandle& Handle, const std::string& name, const NodeConfig& config):StatefulActionNode(name, config)
+    {
+        nh = Handle;
+        auto start = std::chrono::high_resolution_clock::now();
+    }
+    static PortsList providedPorts()
+    {
+        return {};
+    }
+    NodeStatus onStart() override
+    {
+        return BT::NodeStatus::RUNNING;
+    }
+    NodeStatus onRunning() override
+    {
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> diff = end-start;
+        std::cout << "Time from start: " << diff.count() << std::endl;
+        if(diff.count() > 250)
+        {
+            return BT::NodeStatus::SUCCESS;
+        }
+        return BT::NodeStatus::RUNNING;
+    }
+    void onHalted() override
+    {
+        std::cout << "Timeout interrupted" << std::endl;
+    }
+private:
+    ros::NodeHandle nh;
+    std::chrono::time_point<std::chrono::high_resolution_clock> start;
+}
